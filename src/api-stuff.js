b37ff9069c729a7c993507db85c2217589438e18
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-10-13 14:51:44 +0100

    Create game + POST few scores to API

---
+++ src/api-stuff.js
@@ -0,0 +1,3 @@
+const myGameId = 'm4sf6ZHY83BiDA7soyq9';
+const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+const gamesURL = `${baseURL}games/${myGameId}/scores/`;
--- src/data.js
+++ src/data.js
@@ -1,13 +1,13 @@
 const data = [
-  { name: 'King007', score: 23 },
-  { name: 'PaulWalker', score: 18 },
-  { name: 'Gonakie', score: 16 },
-  { name: 'Punisher', score: 30 },
-  { name: 'Microscope', score: 24 },
-  { name: 'RealGuy', score: 13 },
+  { user: 'King007', score: 23 },
+  { user: 'PaulWalker', score: 18 },
+  { user: 'Gonakie', score: 16 },
+  { user: 'Punisher', score: 30 },
+  { user: 'Microscope', score: 24 },
+  { user: 'RealGuy', score: 13 },
 ];
 
-const idFromIndex = (i) => `${data[i].name}-${data[i].score}`;
+const idFromIndex = (i) => `${data[i].user}-${data[i].score}`;
 
 function displayData(data) {
   const lbl = document.querySelector('.lb-list');
@@ -15,7 +15,7 @@ function displayData(data) {
   const codeForData = data.sort((a, b) => b.score - a.score)
     .map((el, i) => `<li id="${idFromIndex(i)}" class="show">
                 <span class="rk">${i + 1}</span>
-                <span class="name">${el.name}</span>
+                <span class="user">${el.user}</span>
                 <span class="score">${el.score}</span>
               </li>`).join('');
   lbl.innerHTML = codeForData;
